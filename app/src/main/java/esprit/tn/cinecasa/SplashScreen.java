3d0d1c55c06d2f00c68854e921caa3ac6b85f53c
ahmedabdelmoula <ahmed.abdelmoula1@esprit.tn>
2017-12-02 15:08:22 +0100

    ss
    ss

--- app/src/main/java/esprit/tn/cinecasa/SplashScreen.java
+++ app/src/main/java/esprit/tn/cinecasa/SplashScreen.java
@@ -1,19 +1,30 @@
 package esprit.tn.cinecasa;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
-public class SplashScreen extends Activity {
+
+public class SplashScreen extends AppCompatActivity implements Animation.AnimationListener {
 
     // Splash screen timer
-    private static int SPLASH_TIME_OUT = 200;
+    private static int SPLASH_TIME_OUT = 10000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ImageView imgPoster = (ImageView) findViewById(R.id.imgLogo);
+        Animation animZoomOut = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in);
+        Animation animZoomOut1 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.move);
+        animZoomOut.setAnimationListener(this);
+        animZoomOut1.setAnimationListener(this);
+        imgPoster.startAnimation(animZoomOut);
+
 
         new Handler().postDelayed(new Runnable() {
 
@@ -35,4 +46,18 @@ public class SplashScreen extends Activity {
         }, SPLASH_TIME_OUT);
     }
 
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
 }
